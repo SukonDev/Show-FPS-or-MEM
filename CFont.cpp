774f7dd0950b4395fcc92918354343dcecc81ac7
GalaxyDev <modmod1122mo@gmail.com>
2025-06-06 01:32:13 +0700

    Update CFont.cpp

--- CFont.cpp
+++ CFont.cpp
@@ -1,3 +1,4 @@
+//GTA SA 1.08
 void GetScreenWidthAndHeight(int* w, int* h) {
 
 	// RwEngineGetCurrentVideoMode
@@ -66,4 +67,79 @@ void CDebugInfo::Draw()
         return;
     }
 #endif
+}
+
+//GTA SA 2.10 64bit
+struct RwVideoMode {
+    int width;
+    int height;
+    int depth;
+    int flags;
+};
+
+void GetScreenWidthAndHeight(int* w, int* h) {
+
+    // RwEngineGetCurrentVideoMode
+    int dwVideoMode = ((int (*)(void))(g_libGTASA + 0x22D0B0))();
+
+    RwVideoMode currentVideoMode;
+
+    // RwEngineGetVideoModeInfo
+    ((void (__fastcall *)(RwVideoMode *, int))(g_libGTASA + 0x26EA88))(&currentVideoMode, dwVideoMode);
+
+    *w = currentVideoMode.width;
+    *h = currentVideoMode.height;
+}
+
+void UI::renderDebug()
+{
+    if(!pSettings->Get().iFPSCounter) return;
+
+    static float fps = 120.f;
+    static auto lastTick = CTimer::m_snTimeInMillisecondsNonClipped;
+    if(CTimer::m_snTimeInMillisecondsNonClipped - lastTick > 500) {
+        lastTick = CTimer::m_snTimeInMillisecondsNonClipped;
+        fps = std::clamp(CTimer::game_FPS, 10.f, 120.f);
+    }
+
+    char szStr[64];
+    snprintf(szStr, sizeof(szStr), "FPS: %.0f", fps);
+
+    auto &msUsed = CStreaming::ms_memoryUsed;
+    auto &msAvailable = CStreaming::ms_memoryAvailable;
+
+    char szStrMem[128];
+    snprintf(szStrMem, sizeof(szStrMem), "MEM: %d/%d MB",
+             msUsed / (1024 * 1024),
+             msAvailable / (1024 * 1024));
+
+    // ตั้งค่าการแสดงผล font
+    CFont::SetBackground(false, false);
+    CFont::SetProportional(true);
+    CFont::SetFontStyle(1);
+    CFont::SetScale(1.4f);  // Note : ถ้า Error  เพิ่ม static void SetScale(float w);
+    CFont::SetOrientation(1);
+    CFont::SetWrapX(640.0f);
+    CFont::SetEdge(0);
+    CFont::SetJustify(false);
+
+    uint32_t bgColor = 0x00000000;     // โปร่งใส 100%
+    uint32_t textColor = 0xFFFFFFFF;   // ขาวเต็ม
+
+    CFont::SetBackgroundColor(&bgColor);
+    CFont::SetColor(&textColor);
+
+
+    int iScreenWidth, iScreenHeight;
+    GetScreenWidthAndHeight(&iScreenWidth, &iScreenHeight);
+
+    CFont::PrintString(
+        ((float)iScreenWidth / 640.0f) * 4.0f,
+        ((float)iScreenHeight / 448.0f) * 200.0f,
+        szStr);
+
+    CFont::PrintString(
+        ((float)iScreenWidth / 640.0f) * 4.0f,
+        ((float)iScreenHeight / 448.0f) * 208.0f,
+        szStrMem);
 }
